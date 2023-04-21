8422c655da5ee632be307fcf7976ce063e4aaa07
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-04-21 13:31:10 -0700

    portfolio work

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './App.css'
-import HomePage from "./components/HomePage"
+import HomePage from './components/HomePage'
 import AboutMe from './components/AboutMe'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
 import Tech from './components/Tech'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
 
     <div className='container'>
+      <Navbar className='navbar'/>
       <HomePage className='home'/>
       <AboutMe className='about'/>
       <Tech className='tech'/>
