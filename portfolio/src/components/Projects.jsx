07bae4944f8d456d9b42ab8989bd97c6c51072e2
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-06-04 15:56:41 -0700

    inprogress

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
-
-
+import React from "react"
 
 export const Projects = () => {
-  return (
-
-  <div className='projects'>
-    <div>Projects</div>
-  </div>
-  )
+	return (
+		<div className="projects">
+			<h1>Projects</h1>
+			<div className="tictac">
+				<h2>Tic Tac Toe</h2>
+				<img className="ttt" src="./techpic/TTT.png" />
+				<h3>
+					This was the first project that I deployed while studying at LEARN
+					Academy. I collaborated with a fellow student to build this multiplayer Tic Tac Toe
+					application using React.
+				</h3>
+			</div>
+			<div className="treasure">
+				<h2>Treasure Hunt</h2>
+				<img className="soon" src="./techpic/SOON.png" />
+				<h3>
+					Before I fully started my web developement journey, I took a weekend web developer workshop that showed us how to build a simple "Treasure hunt" game using JavaScript, HTML, and CSS. I decided to revisit this project so that I could refactor the game using React.
+				</h3>
+			</div>
+			<div className="plt">
+				<h2>Pig Latin Translator</h2>
+				<img className="soon" src="./techpic/SOON.png" />
+				<h3>
+					{" "}
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea excepturi
+					non perferendis consectetur veniam reprehenderit similique culpa,
+				</h3>
+			</div>
+			<div className="tinder">
+				<h2>Cat Tinder</h2>
+				<img className="soon" src="./techpic/SOON.png" />
+				<h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+					quibusdam doloremque veniam ad, ex laudantium molestias autem qui
+				</h3>
+			</div>
+			<div className="cap">
+				<h2>Capstone Project</h2>
+				<img className="soon" src="./techpic/SOON.png" />
+				<h3>
+					{" "}
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore quod
+					vero asperiores corporis fugit, totam, tempore dolor omnis et at
+				</h3>
+			</div>
+		</div>
+	)
 }
-
 
 export default Projects
--- portfolio/src/components/Tech.jsx
+++ portfolio/src/components/Tech.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-
-
+import React from "react"
 
 export const Tech = () => {
-  
-  return (
-  <div className='tech'>
-    <div>Tech</div>
-  </div>
-  )
+	return (
+		<div className="tech">
+      {/* <div className="tech-title">
+			<h1>Skills</h1> 
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>RUBY</li>
+          <li>React</li>
+          <li>RUBY ON RAILS</li>
+          <li>POSTGRESQL</li>
+          <li>JEST</li>
+          <li>NODEJS</li>
+          <li>POSTMAN</li>
+          <li>GITHUB</li>
+          <li>SLACK</li>
+        </ul>
+      
+      </div> */}
+			<div className="stack">
+				<img src="./techpic/REACT.png" />
+			</div>
+		</div>
+	)
 }
-
-
 export default Tech
